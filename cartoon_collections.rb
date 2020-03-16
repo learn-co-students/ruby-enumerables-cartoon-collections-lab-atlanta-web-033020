6996a234a5506748248100b086d874bd5cf1193e
Wendy Raven McNair <wen.mcnair@gmail.com>
2020-03-16 03:34:52 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,55 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(array) # code an argument here
   # Your code here
+  
+                      
+                          array.each_with_index{ |name, i|
+                            puts "#{i+1} #{name}"
+                          }
 end
 
-def summon_captain_planet# code an argument here
+
+
+
+
+def summon_captain_planet(array) # code an argument here
   # Your code here
+  
+                          call = []
+                          array.map { |i| call << "#{i.capitalize}!"}
+                    call
+  
 end
 
-def long_planeteer_calls# code an argument here
+
+
+
+
+def long_planeteer_calls(array) # code an argument here
   # Your code here
+  
+                          array.any? do |i|
+                            if i.length > 4
+                              return true
+                            end
+                          end  
+                            false
 end
 
-def find_the_cheese# code an argument here
+
+
+
+
+def find_the_cheese(array) # code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
+                      array.detect do |current_el|
+                        cheese_types.include?(current_el) 
+                      end
+                
 end
+
+
+
+
+
